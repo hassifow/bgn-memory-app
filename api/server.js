469a78cc26d94c24ae918ad5a00c342f8e130a29
Hassif-Abdulahi-Mustafa <asif118@icloud.com>
2019-01-26 13:57:37 +0000

    update on google cloud speech
    Configured google cloud speech, to generate speech to text file.

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+
+let cloud_speech =  require('./cloud-speech');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
+  let a = cloud_speech.detectSpeechInAudioFile();
   res.json("HELLO TO BGN MEMORY APP API!")
 });
 app.post('/api/world', (req, res) => {
