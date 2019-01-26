5d8d01d87f262a8bdc1a4bd224e59a736e912900
Ola <ola.otaiku@sky.uk>
2019-01-26 15:46:12 +0000

    removed uncommented lines of code

--- api/cloud-speech.js
+++ api/cloud-speech.js
@@ -5,27 +5,6 @@ const fs = require("fs");
 // Creates a client
 const client = new speech.SpeechClient();
 
-// // The name of the audio file to transcribe
-// const fileName = "./resources/test1.wav";
-
-// // Reads a local audio file and converts it to base64
-// const file = fs.readFileSync(fileName);
-// const audioBytes = file.toString("base64");
-
-// // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-// const audio = {
-//   content: audioBytes
-// };
-// const config = {
-//   encoding: "LINEAR16",
-//   sampleRateHertz: 44100,
-//   languageCode: "en-US"
-// };
-// const request = {
-//   audio: audio,
-//   config: config
-// };
-
 // Detects speech in the audio file
 function detectSpeechInAudioFile(fileName) {
 
