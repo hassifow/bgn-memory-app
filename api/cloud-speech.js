6d7d1870f38f9cda569d45677c7d617ddfc3db74
Ola <ola.otaiku@sky.uk>
2019-01-26 15:33:59 +0000

    refactored cloud-speech.js to convert for a given file name

--- api/cloud-speech.js
+++ api/cloud-speech.js
@@ -1,54 +1,62 @@
-
-
 // Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
+const speech = require("@google-cloud/speech");
+const fs = require("fs");
 
 // Creates a client
 const client = new speech.SpeechClient();
 
-// The name of the audio file to transcribe
-const fileName = './resources/test1.wav';
+// // The name of the audio file to transcribe
+// const fileName = "./resources/test1.wav";
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
+// // Reads a local audio file and converts it to base64
+// const file = fs.readFileSync(fileName);
+// const audioBytes = file.toString("base64");
 
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const audio = {
-  content: audioBytes,
-};
-const config = {
-  encoding: 'LINEAR16',
-  sampleRateHertz: 44100,
-  languageCode: 'en-US',
-};
-const request = {
-  audio: audio,
-  config: config,
-};
-
-
+// // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+// const audio = {
+//   content: audioBytes
+// };
+// const config = {
+//   encoding: "LINEAR16",
+//   sampleRateHertz: 44100,
+//   languageCode: "en-US"
+// };
+// const request = {
+//   audio: audio,
+//   config: config
+// };
 
 // Detects speech in the audio file
-function detectSpeechInAudioFile (){
-  return new Promise((resolve, reject) => {
-  client
-    .recognize(request)
-    .then(data => {
-      const response = data[0];
-      const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
-      resolve (transcription);
-      console.log(`Transcription: ${transcription}`);
-    })
-    .catch(err => {
-      resolve (err);
-      console.error('ERROR:', err);
+function detectSpeechInAudioFile(fileName) {
 
-    });
-    });
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString("base64");
+
+  const audio = {
+    content: audioBytes
+  };
+
+  const request = {
+    audio: audio,
+    config: config
+  };
+
+  return new Promise((resolve, reject) => {
+    client
+      .recognize(request)
+      .then(data => {
+        const response = data[0];
+        const transcription = response.results
+          .map(result => result.alternatives[0].transcript)
+          .join("\n");
+        resolve(transcription);
+        console.log(`Transcription: ${transcription}`);
+      })
+      .catch(err => {
+        resolve(err);
+        console.error("ERROR:", err);
+      });
+  });
 }
 
 module.exports.detectSpeechInAudioFile = detectSpeechInAudioFile;
--- api/server.js
+++ api/server.js
@@ -7,19 +7,49 @@ let cloud_speech =  require('./cloud-speech');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//GET REQUESTS
 app.get('/api/hello', (req, res) => {
+    res.json("Welcome to PreParrot API")
+  }
+);
+
+app.get('/speech/audio/test', (req, res) => {
   cloud_speech.detectSpeechInAudioFile().then((a) => {
-    console.log(a);
     res.json(a)
   }
 );
 
+app.get('/speech/audio/:id', (req, res) => {
+  cloud_speech.detectSpeechInAudioFile().then((a) => {
+    res.json(a)
+  }
+);
+
+//POST REQUESTS
+app.post('/speech/text', (req, res) => {
+
+  const text = req.body.text;
+
+  if(text === null){
+    res.status(404).send("text not included in the body");
+  }
+  else{
+    //store text in DB
+      res.status(200).json({
+        message: "text sucessfully uploaded",
+        id: "to be returned from database"
+      })
+    }
 });
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+
+app.post('/speech/audio', (req, res) => {
+//upload to /resources foler with name saved as ID
+//save url path 
+//connect to cloud speech api and return generated text
+
 });
+
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
